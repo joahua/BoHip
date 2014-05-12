94fe7ba0614ab17e4de1f5e9527848eeab58ae7f
Josh Street <josh@josh.st>
2014-05-13 08:27:44 +1000

    Nearly flippable

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ function generateTileBackground () {
 	
 	// Aesthetics
 	var trianglifyOpts = {
-		noiseIntensity: .1, 
+		noiseIntensity: 0.1, 
 		cellpadding: 10, 
 		cellsize: 50
 	};
-	var css = '.bogan { background-image: ' + new Trianglify(trianglifyOpts)
+	var css = '.card.bogan { background-image: ' + new Trianglify(trianglifyOpts)
 						.generate(250, 250)
 						.dataUrl + ' }' +
-			  '.hipster { background-image: ' + new Trianglify(trianglifyOpts)
+				'.card.hipster { background-image: ' + new Trianglify(trianglifyOpts)
 						.generate(250, 250)
 						.dataUrl + ' }',
-	    head = document.head,
-	    style = document.createElement('style');
+		head = document.head,
+		style = document.createElement('style');
 
 	var tileBg = document.getElementById('tileBg');
 	if (tileBg) head.removeChild(tileBg);
 
 	style.type = 'text/css';
 	if (style.styleSheet){
-	  style.styleSheet.cssText = css;
+		style.styleSheet.cssText = css;
 	} else {
-	  style.appendChild(document.createTextNode(css));
+		style.appendChild(document.createTextNode(css));
 	}
 
 	style.id = "tileBg";
@@ -32,7 +32,35 @@ function generateTileBackground () {
 }
 
 generateTileBackground();
-document.addEventListener('click', generateTileBackground);
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	document.querySelector('.new-color')
+	.addEventListener('click', generateTileBackground);
+
+	var lastState = 0;
+	var cards = document.querySelector('#cards');
+
+	document.querySelector('.both-cards')
+	.addEventListener('click', function() {
+		cards.className = 'hipster bogan'
+	})
+
+	document.querySelector('.toggle-cards')
+	.addEventListener('click', function() {
+
+		var states = [
+			'hipster'
+			, 'bogan'
+			// , 'hipster bogan'
+		];
+
+		cards.className = states[lastState++] || states[0];
+
+		if (lastState >= states.length)
+			lastState = 0;
+	});
+});
 
 // Main card template
 var card = Hogan.compile("<card class='card {{Classes}}'>"+
@@ -45,7 +73,7 @@ var card = Hogan.compile("<card class='card {{Classes}}'>"+
 
 // Get CSV
 d3.csv("data.csv", function(err, rows) {
-	var page = d3.select('body');
+	var page = d3.select('#cards');
 
 	var cards = [];
 	rows.map(function(row) {
@@ -58,7 +86,7 @@ d3.csv("data.csv", function(err, rows) {
 
 			var count = (row.Counter > 1) ? (i+1) + " of " + row.Counter : '';
 
-			cards.push(
+			cards.push("<pair>",
 				// Bogan
 				card.render({
 					Title: titlify(row.Bogan),
@@ -73,7 +101,8 @@ d3.csv("data.csv", function(err, rows) {
 					Classes: classes + ' hipster',
 					Category: row.Category,
 					Counter: count
-				})
+				}),
+				"</pair>"
 			);
 		}
 	});
